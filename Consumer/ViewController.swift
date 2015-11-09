620c26387498d5ca7902e6ffa8d693ed8e1c98cc
Dan Stepanov <danhstepanov@gmail.com>
2015-11-08 20:56:15 -0800

    Initial Commit

---
+++ Consumer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Consumer
+//
+//  Created by Daniel Stepanov on 11/8/15.
+//  Copyright © 2015 Daniel Stepanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
